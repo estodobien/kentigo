5750d5578346303f1eef873565178647525819a2
Anton <estodobien000@gmail.com>
2026-07-16 00:48:04 +0200

    2

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://csffnyrhyxmlcfdzngfx.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://csffnyrhyxmlcfdzngfx.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_nTD-LDImtmgZvSgG2zPGoA_2UMo4Pn9";
 
 const supabase = window.supabase.createClient(
